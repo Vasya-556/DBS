a6e6a5a06b9158c80907e4d22a2d3ae596275df8
Vasya-556 <vkobrin556@gmail.com>
2023-10-03 00:06:47 +0300

    Add

---
+++ lab4/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace lab4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.showcaseBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this._5semDataSet = new lab4._5semDataSet();
+            this.semDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.showcaseTableAdapter = new lab4._5semDataSetTableAdapters.showcaseTableAdapter();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.detailDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.idsDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.showcaseBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this._5semDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.semDataSetBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idsDataGridViewTextBoxColumn,
+            this.detailDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.showcaseBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(433, 150);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.SelectionChanged += new System.EventHandler(this.dataGridView1_SelectionChanged);
+            // 
+            // showcaseBindingSource
+            // 
+            this.showcaseBindingSource.DataMember = "showcase";
+            this.showcaseBindingSource.DataSource = this._5semDataSet;
+            // 
+            // _5semDataSet
+            // 
+            this._5semDataSet.DataSetName = "_5semDataSet";
+            this._5semDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // semDataSetBindingSource
+            // 
+            this.semDataSetBindingSource.DataSource = this._5semDataSet;
+            this.semDataSetBindingSource.Position = 0;
+            // 
+            // showcaseTableAdapter
+            // 
+            this.showcaseTableAdapter.ClearBeforeFill = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 181);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(146, 127);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(451, 40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(323, 314);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 51);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Select image";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(299, 181);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(146, 127);
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(451, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 5;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(451, 69);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Refresh";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // detailDataGridViewTextBoxColumn
+            // 
+            this.detailDataGridViewTextBoxColumn.DataPropertyName = "detail";
+            this.detailDataGridViewTextBoxColumn.HeaderText = "detail";
+            this.detailDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.detailDataGridViewTextBoxColumn.Name = "detailDataGridViewTextBoxColumn";
+            this.detailDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // idsDataGridViewTextBoxColumn
+            // 
+            this.idsDataGridViewTextBoxColumn.DataPropertyName = "id_s";
+            this.idsDataGridViewTextBoxColumn.HeaderText = "id_s";
+            this.idsDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.idsDataGridViewTextBoxColumn.Name = "idsDataGridViewTextBoxColumn";
+            this.idsDataGridViewTextBoxColumn.ReadOnly = true;
+            this.idsDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(451, 98);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Delete";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.showcaseBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this._5semDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.semDataSetBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource semDataSetBindingSource;
+        private _5semDataSet _5semDataSet;
+        private System.Windows.Forms.BindingSource showcaseBindingSource;
+        private _5semDataSetTableAdapters.showcaseTableAdapter showcaseTableAdapter;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idsDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn detailDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ lab4/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab4
+{
+    public partial class Form1 : Form
+    {
+        string connectionString = "Data Source=localhost\\SQLEXPRESS;Initial Catalog=5sem;Integrated Security=True";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the '_5semDataSet.showcase' table. You can move, or remove it, as needed.
+            this.showcaseTableAdapter.Fill(this._5semDataSet.showcase);
+
+        }
+
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                // Get the selected row
+                DataGridViewRow selectedRow = dataGridView1.SelectedRows[0];
+
+                // Access the underlying data source object (assuming it's a DataRow)
+                DataRowView dataRowView = (DataRowView)selectedRow.DataBoundItem;
+
+                // Assuming you have an "image" column in your DataTable
+                byte[] imageData = (byte[])dataRowView["image"];
+
+                if (imageData != null && imageData.Length > 0)
+                {
+                    using (MemoryStream stream = new MemoryStream(imageData))
+                    {
+                        // Load the image from the byte array and display it in the PictureBox
+                        pictureBox1.Image = Image.FromStream(stream);
+                    }
+                }
+                else
+                {
+                    // Clear the PictureBox if no image is available for the selected row
+                    pictureBox1.Image = null;
+                }
+            }
+            else
+            {
+                // Clear the PictureBox if no row is selected
+                pictureBox1.Image = null;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Image Files (*.jpg; *.jpeg; *.png; *.gif)|*.jpg; *.jpeg; *.png; *.gif";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    pictureBox2.Image = Image.FromFile(openFileDialog.FileName);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox1.Text))
+            {
+                MessageBox.Show("Please enter a detail.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Check if an image is selected
+            if (pictureBox2.Image == null)
+            {
+                MessageBox.Show("Please select an image.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            try
+            {
+                // Convert the image to a byte array
+                byte[] imageData;
+                using (MemoryStream stream = new MemoryStream())
+                {
+                    pictureBox2.Image.Save(stream, ImageFormat.Jpeg); 
+                    imageData = stream.ToArray();
+                }
+
+                // Insert the data into the database
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    string query = "INSERT INTO showcase (detail, image) VALUES (@detail, @image)";
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@detail", textBox1.Text);
+                        command.Parameters.AddWithValue("@image", imageData);
+                        command.ExecuteNonQuery();
+                    }
+                }
+                textBox1.Text = "";
+                pictureBox2.Image = null;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = "SELECT * FROM showcase";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+                    {
+                        DataTable dataTable = new DataTable();
+                        adapter.Fill(dataTable);
+
+                        dataGridView1.DataSource = dataTable;
+                    }
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string valueToDelete = textBox1.Text;
+
+                string query = $"DELETE FROM showcase WHERE id_s = @ValueToDelete";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@ValueToDelete", valueToDelete);
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
