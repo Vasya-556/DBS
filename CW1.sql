a497b3f925ca388c3856e18bf2d6e19a5c2ef4df
Vasya-556 <vkobrin556@gmail.com>
2023-10-16 19:40:36 +0300

    Add

---
+++ CW1.sql
@@ -0,0 +1,52 @@
+﻿CREATE TABLE Products (
+   Product_ID INT PRIMARY KEY IDENTITY(1,1),
+   Name VARCHAR(255),
+   Price INT,
+   FK_Category INT
+);
+
+CREATE TABLE Categories (
+    Category_ID INT PRIMARY KEY IDENTITY(1,1),
+    Name VARCHAR(50)
+);
+
+CREATE TABLE Sales (
+    Sale_ID INT PRIMARY KEY IDENTITY(1,1),
+    Quantity INT,
+    Sale_Date DATE,
+    FK_Product INT
+);
+
+ALTER TABLE Products
+ADD CONSTRAINT FK_Category FOREIGN KEY (FK_Category)
+REFERENCES Categories(Category_ID);
+
+ALTER TABLE Sales
+ADD CONSTRAINT FK_Product FOREIGN KEY (FK_Product)
+REFERENCES Products(Product_ID);
+
+INSERT INTO Products (Name, Price, FK_Category)
+VALUES
+    ('Smartphone', 500, 1),
+    ('LED TV', 800, 1),
+    ('Refrigerator', 700, 2);
+
+INSERT INTO Categories (Name)
+VALUES
+    ('Electronics'),
+    ('Home Appliances'),
+    ('Lighting');
+
+INSERT INTO Sales (Quantity, Sale_Date, FK_Product)
+VALUES
+    (2, '2023-10-04', 1),
+    (1, '2023-10-04', 2),
+    (3, '2023-10-04', 3);
+
+SELECT * FROM Products
+SELECT * FROM Sales
+SELECT * FROM Categories
+
+DROP TABLE Products
+DROP TABLE Sales
+DROP TABLE Categories
---
+++ lab6.sql
@@ -0,0 +1,64 @@
+﻿CREATE TABLE Products (
+    product_id INT IDENTITY(1,1) PRIMARY KEY,
+    name NVARCHAR(255),
+    price DECIMAL(10, 2),
+    category_id INT,
+    supplier_id INT
+);
+
+CREATE TABLE Categories (
+    category_id INT IDENTITY(1,1) PRIMARY KEY,
+    name NVARCHAR(255)
+);
+
+CREATE TABLE Suppliers (
+    supplier_id INT IDENTITY(1,1) PRIMARY KEY,
+    name NVARCHAR(255),
+    contact_info NVARCHAR(255)
+);
+
+CREATE TABLE Orders (
+    order_id INT IDENTITY(1,1) PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    status NVARCHAR(50)
+);
+
+CREATE TABLE Customers (
+    customer_id INT IDENTITY(1,1) PRIMARY KEY,
+    first_name NVARCHAR(50),
+    last_name NVARCHAR(50),
+    email NVARCHAR(100)
+);
+
+ALTER TABLE Products
+ADD FOREIGN KEY (category_id) REFERENCES Categories(category_id);
+
+ALTER TABLE Products
+ADD FOREIGN KEY (supplier_id) REFERENCES Suppliers(supplier_id);
+
+ALTER TABLE Orders
+ADD FOREIGN KEY (customer_id) REFERENCES Customers(customer_id);
+
+INSERT INTO Products (name, price, category_id, supplier_id)
+VALUES ('Product 1', 10.99, 1, 1),   
+       ('Product 2', 20.49, 2, 2),
+       ('Product 3', 5.99, 1, 3);
+
+INSERT INTO Categories (name)
+VALUES ('Category 1'),
+       ('Category 2');
+
+INSERT INTO Suppliers (name, contact_info)
+VALUES ('Supplier A', 'Contact Info A'),
+       ('Supplier B', 'Contact Info B'),
+       ('Supplier C', 'Contact Info C');
+
+INSERT INTO Orders (customer_id, order_date, status)
+VALUES (1, '2023-10-15', 'in processing'),
+       (2, '2023-10-16', 'sent');
+
+INSERT INTO Customers (first_name, last_name, email)
+VALUES ('John', 'Doe', 'john.doe@example.com'),
+       ('Jane', 'Smith', 'jane.smith@example.com');
+
